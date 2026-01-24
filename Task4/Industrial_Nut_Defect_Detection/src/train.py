7a568208b22c830a94df9208a8f331544f597434
kimyujiin <kyi1380@naver.com>
2026-01-24 16:12:46 +0900

    ADD: results.png, best.pt, update src

--- Task4/Industrial_Nut_Defect_Detection/src/train.py
+++ Task4/Industrial_Nut_Defect_Detection/src/train.py
@@ -8,8 +8,7 @@ results = model.train(
 	data='../data/data.yaml',  # 설정 파일 경로
 	epochs=50,  # 전체 데이터를 반복 학습 횟수
 	imgsz=640,  # 이미지 크기
-	batch=4,  # 한 번에 학습할 이미지 개수
-	workers = 0,  # 충돌 방지
+	batch=32,  # 한 번에 학습할 이미지 개수
 	name='nut_defect_model',  # 결과가 저장될 폴더 이름
-	device='cpu'  # GPU가 없다면 'cpu', 있다면 0
+	device=0  # T4 GPU
 )
