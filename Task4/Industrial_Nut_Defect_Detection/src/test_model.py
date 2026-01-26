f82bd69d8ae01b20fe3e9100d8da5198fff1ac74
kimyujiin <kyi1380@naver.com>
2026-01-26 20:47:44 +0900

    ADD: best.pt, results.png

--- Task4/Industrial_Nut_Defect_Detection/src/test_model.py
+++ Task4/Industrial_Nut_Defect_Detection/src/test_model.py
@@ -5,12 +5,17 @@ import cv2
 model = YOLO('../models/best.pt') 
 
 # 2. 테스트할 이미지 경로
-img_path = '../data/dataset/images/val/scratch_002.png' 
+img_list = [
+	'../data/dataset/images/val/test_good_rep0_014.png',
+	'../data/dataset/images/val/test_color_rep0_020.png',
+	'../data/dataset/images/val/test_flip_rep0_018.png'
+]
 
 # 3. 모델로 예측하기
-results = model.predict(source=img_path, save=True, conf=0.25)
+results = model.predict(source=img_list, save=True, conf=0.25)
 
 # 4. 결과 확인
-for result in results:
+for i, result in enumerate(results):
+	print(f"--- Image {i+1} ---")
 	print(f"Detected: {len(result.boxes)} objects")
 	result.show()
